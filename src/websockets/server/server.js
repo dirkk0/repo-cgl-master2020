08451366891c63030589fa4552218bece435c0c6
Dirk Krause <dirkk0@googlemail.com>
2021-07-14 18:45:11 +0200

    first socket connection

--- src/websockets/server/server.js
+++ src/websockets/server/server.js
@@ -5,21 +5,24 @@ const express = require('express')  // this does fancy web stuff
 
 const app = express() // defines the web app
 
-const server = http.createServer(app)
+// first install
+// npm install socket.io
+const socketio = require('socket.io');
 
-// let app = express()
-
-// this creates a server instance
-// let server = http.createServer(function (req, res) {
-//     res.end('hello world', req.url) // this returns a message as a result
-//     console.log(req.url)
-// })
+const server = http.createServer(app) // create the server
 
 app.use('/', express.static(__dirname + '/../public'));
-console.log(__dirname + '/public')
+console.log(__dirname + '/public');
 
 const PORT=5000 // this defines the port
 
 console.log("server started on port", PORT)
 
 server.listen(PORT) // this start the actual server
+
+const io = socketio(server);
+
+io.on('connection', (socket) => {
+    console.log("connection established")
+})
+
