fea9431a3299ebd017112b356695af438fdd02e7
Dirk Krause <dirkk0@googlemail.com>
2021-07-14 18:34:58 +0200

    first html page

--- src/websockets/server/server.js
+++ src/websockets/server/server.js
@@ -5,13 +5,18 @@ const express = require('express')  // this does fancy web stuff
 
 const app = express() // defines the web app
 
+const server = http.createServer(app)
+
 // let app = express()
 
 // this creates a server instance
-let server = http.createServer(function (req, res) {
-    res.end('hello world', req.url) // this returns a message as a result
-    console.log(req.url)
-})
+// let server = http.createServer(function (req, res) {
+//     res.end('hello world', req.url) // this returns a message as a result
+//     console.log(req.url)
+// })
+
+app.use('/', express.static(__dirname + '/../public'));
+console.log(__dirname + '/public')
 
 const PORT=5000 // this defines the port
 
